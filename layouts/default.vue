802c87c1d3aeea7e2748acad48825071af7d323e
Brayan <brayanespitia10@gmail.com>
2023-09-06 08:45:40 -0500

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-app>
+        <v-app-bar>
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer"> </v-app-bar-nav-icon>
+            <v-spacer />
+        </v-app-bar>
+        <v-navigation-drawer v-model="drawer">
+            <v-list>
+                <v-list-item title="Navigation drawer"></v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+
+
+
+        <v-main>
+            <slot />
+        </v-main>
+    </v-app>
+</template>
+
+
+<script setup>
+
+const drawer = ref(false)
+</script>
